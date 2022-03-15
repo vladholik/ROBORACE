d20068f7ac91fa4e79402f6e4578060f41cb112c
Vladislav_Golik <vladholik@yandex.ru>
2022-03-15 16:08:02 +0300

    TOF files adde

---
+++ Program/rb20220314/TOF.c
@@ -0,0 +1,135 @@
+#include <VL53L0X.h>
+#include <Wire.h>
+
+VL53L0X tof_0, tof_1, tof_2, tof_3, tof_4, tof_5;
+
+struct tofs
+{
+  int l_90, l_45, l_0, r_0, r_45, r_90;
+};
+
+tofs tof_distance;
+
+void TCA9548A(uint8_t bus)
+{
+  Wire.beginTransmission(0x70); // TCA9548A адрес 0x70
+  Wire.write(1 << bus); // отправляем байт на выбранную шину
+  Wire.endTransmission();
+}
+
+bool setup_6_tofs(int timeout) {
+
+  TCA9548A(0);
+  Serial.println("Подготовка tof-модулей ...");
+  if (tof_0.init() == 0) {
+    Serial.println("Не удалось обнаружить и инициализировать tof-модуль 0!");
+    return 0;
+  }
+  tof_0.setTimeout(timeout);
+  tof_0.setSignalRateLimit(0.1);
+  tof_0.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  tof_0.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+  tof_0.startContinuous();
+
+  TCA9548A(1);
+  if (tof_1.init() == 0) {
+    Serial.println("Не удалось обнаружить и инициализировать tof-модуль 1!");
+    return 0;
+  }
+  tof_1.setTimeout(timeout);
+  tof_1.startContinuous();
+
+  TCA9548A(2);
+  if (tof_2.init() == 0) {
+    Serial.println("Не удалось обнаружить и инициализировать tof-модуль 2!");
+    return 0;
+  }
+  tof_2.setTimeout(timeout);
+  tof_2.startContinuous();
+
+  TCA9548A(3);
+  if (tof_3.init() == 0) {
+    Serial.println("Не удалось обнаружить и инициализировать tof-модуль 3!");
+    return 0;
+  }
+  tof_3.setTimeout(timeout);
+  tof_3.setSignalRateLimit(0.1);
+  tof_3.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  tof_3.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+  tof_3.startContinuous();
+
+  TCA9548A(4);
+  if (tof_4.init() == 0) {
+    Serial.println("Не удалось обнаружить и инициализировать tof-модуль 4!");
+    return 0;
+  }
+  tof_4.setTimeout(timeout);
+  tof_4.startContinuous();
+
+
+  TCA9548A(7);
+  if (tof_5.init() == 0) {
+    Serial.println("Не удалось обнаружить и инициализировать tof-модуль 5!");
+    return 0;
+  }
+  tof_5.setTimeout(timeout);
+  tof_5.startContinuous();
+
+  Serial.println("success");
+  return 0;
+}
+
+tofs tof_read() {
+
+  TCA9548A(0);
+  if (0 != (tof_0.readReg(RESULT_INTERRUPT_STATUS) & 0x07)) {
+    tof_distance.r_0 = tof_0.readReg16Bit(RESULT_RANGE_STATUS + 10);
+    tof_0.writeReg(SYSTEM_INTERRUPT_CLEAR, 0x01);
+    //Serial.println(r_0);
+    if (tof_distance.r_0 > 2200)
+      tof_distance.r_0 = 2200;
+  }
+
+  TCA9548A(1);
+  if (0 != (tof_1.readReg(RESULT_INTERRUPT_STATUS) & 0x07)) {
+    tof_distance.l_45 = tof_1.readReg16Bit(RESULT_RANGE_STATUS + 10);
+    tof_1.writeReg(SYSTEM_INTERRUPT_CLEAR, 0x01);
+    if (tof_distance.l_45 > 1200)
+      tof_distance.l_45 = 1200;
+  }
+
+  TCA9548A(2);
+  if (0 != (tof_2.readReg(RESULT_INTERRUPT_STATUS) & 0x07)) {
+    tof_distance.r_45 = tof_2.readReg16Bit(RESULT_RANGE_STATUS + 10);
+    tof_2.writeReg(SYSTEM_INTERRUPT_CLEAR, 0x01);
+    if (tof_distance.r_45 > 1200)
+      tof_distance.r_45 = 1200;
+  }
+
+  TCA9548A(3);
+  if (0 != (tof_3.readReg(RESULT_INTERRUPT_STATUS) & 0x07)) {
+    tof_distance.l_0 = tof_3.readReg16Bit(RESULT_RANGE_STATUS + 10);
+    tof_3.writeReg(SYSTEM_INTERRUPT_CLEAR, 0x01);
+    if (tof_distance.l_0 > 2200)
+      tof_distance.l_0 = 2200;
+  }
+
+  TCA9548A(4);
+  if (0 != (tof_4.readReg(RESULT_INTERRUPT_STATUS) & 0x07)) {
+    tof_distance.l_90 = tof_4.readReg16Bit(RESULT_RANGE_STATUS + 10);
+    tof_4.writeReg(SYSTEM_INTERRUPT_CLEAR, 0x01);
+    if (tof_distance.l_90 > 1200)
+      tof_distance.l_90 = 1200;
+  }
+
+  TCA9548A(7);
+  if (0 != (tof_5.readReg(RESULT_INTERRUPT_STATUS) & 0x07)) {
+    tof_distance.r_90 = tof_5.readReg16Bit(RESULT_RANGE_STATUS + 10);
+    tof_5.writeReg(SYSTEM_INTERRUPT_CLEAR, 0x01);
+    if (tof_distance.r_90 > 1200)
+      tof_distance.r_90 = 1200;
+  }
+
+  
+  return tof_distance;
+}
---
+++ Program/rb20220314/TOF.h
@@ -0,0 +1,10 @@
+struct tofs
+{
+  int l_90, l_45, l_0, r_0, r_45, r_90;
+};
+
+void TCA9548A(uint8_t bus);
+
+bool setup_6_tofs(int timeout);
+
+tofs tof_read();
